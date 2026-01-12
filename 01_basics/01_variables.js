8e27c289a5ae91b4a87c071874b046789a344996
Sameer Ram <sameerram9321@gmail.com>
2026-01-12 19:28:04 +0530

    Basic and Variables of JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14457;
+let accountEmail = "sameer@google.com";
+var accountPass = "12345";
+accountCity = "Durgapur";
+
+
+//accountId = 2 //constant variables are not allowed to change
+
+accountEmail = "sam@Gmail.com";
+accountPass = "242628";
+accountCity = "Kolkata";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPass,accountCity]);
