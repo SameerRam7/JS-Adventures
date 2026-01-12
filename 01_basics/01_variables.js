baca91d78655a0615b872700d93ba2f323d4e2f8
Sameer Ram <sameerram9321@gmail.com>
2026-01-12 19:40:24 +0530

    Basic and Variables of JS

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 14457;
 let accountEmail = "sameer@google.com";
 var accountPass = "12345";
-accountCity = "Durgapur";
+accountCity = "Durgapur"; //It's not a good practice
 
 
 //accountId = 2 //constant variables are not allowed to change
@@ -11,5 +11,9 @@ accountPass = "242628";
 accountCity = "Kolkata";
 
 console.log(accountId);
+
+/* 
+Prefer not to use var because of issue in block scope and functional scope
+*/
 
 console.table([accountId,accountEmail,accountPass,accountCity]);
