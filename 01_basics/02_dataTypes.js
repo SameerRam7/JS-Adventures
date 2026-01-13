d2e1e5d56314d0de9cddec987b8c7c374ffce4bc
Sameer Ram <sameerram9321@gmail.com>
2026-01-13 21:00:50 +0530

    DataTypes and its typeof

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) //we're using node.js, not browser
+
+console.log(3
+    +
+        3) // code readability should be high
+
+console.log("Sameer");
+
+let name = "Ram";
+let age = 24;
+let isLoggedIn = false;
+
+//Primitive datatypes
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+
+
+// object 
+
+console.log(typeof("Sameer"));
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(null)); //object
+console.log(typeof(undefined)); //undefined
