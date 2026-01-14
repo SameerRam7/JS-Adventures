74f9e2b9d46a294cab4dfb9a0ce99e6cc4121894
Sameer Ram <sameerram9321@gmail.com>
2026-01-14 23:39:26 +0530

    DataType Conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+//Number
+let score = "Sameer";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//Converting string into number 
+//"33" => 33
+//"33ad" => NaN -> Not a Number
+//true => 1 , false => 0
+
+//Boolean
+let isLoggedIn = "Sameer";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false
+//"" => false
+//"Sameer" => true
+
+//String
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
